c07adbfe6702e3b6c37b4fc4ca30d7008daa99dc
sik9252 <sik9252@naver.com>
2024-08-23 17:26:40 +0900

    refactor: 테스트명 수정

--- components/ui/Button/button.test.tsx
+++ components/ui/Button/button.test.tsx
@@ -1,12 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import { composeStories } from "@storybook/react";
-import { userEvent, within, expect } from "@storybook/test";
+import { expect } from "@storybook/test";
 import * as stories from "./button.stories";
-import { QueryClientWrapper } from "@/utils";
 
 const { Default } = composeStories(stories);
 
-test("Default 속성을 가진 버튼이 화면에 보인다.", () => {
+test("Default", () => {
   render(<Default {...Default.args} />);
   const buttonElement = screen.getByText(/Default Button/i);
   expect(buttonElement).toHaveTextContent(/Default Button/i);
