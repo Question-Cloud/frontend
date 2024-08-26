638edcd59d2e55ca0a2967bbfd6ec47f80f35db1
sik9252 <sik9252@naver.com>
2024-08-26 12:41:39 +0900

    Docs: Input 스토리 추가

---
+++ components/ui/Input/input.stories.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useRef } from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import { Input, InputProps } from "./input";
+
+const meta: Meta<InputProps> = {
+  title: "Components/Input",
+  component: Input,
+  argTypes: {
+    type: {
+      control: { type: "select" },
+      options: ["text", "password"],
+    },
+    placeholder: {
+      control: { type: "text" },
+    },
+    disabled: {
+      control: "boolean",
+    },
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<InputProps>;
+
+export const Default: Story = {
+  args: {
+    type: "text",
+    placeholder: "텍스트를 입력해주세요",
+  },
+};
+
+export const Password: Story = {
+  args: {
+    type: "password",
+    placeholder: "비밀번호를 입력해주세요",
+  },
+};
+
+export const Labeled: Story = {
+  args: {
+    type: "text",
+    placeholder: "라벨이 있는 Input",
+    label: "라벨명",
+  },
+};
+
+export const Required: Story = {
+  args: {
+    type: "text",
+    placeholder: "필수 입력 Input",
+    label: "필수값",
+    isRequired: true,
+  },
+};
+
+export const Focus: StoryObj<InputProps> = {
+  render: (args) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+      if (inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, []);
+
+    return <Input {...args} ref={inputRef} />;
+  },
+  args: {
+    type: "text",
+    className: "focus:border-gray_04",
+    placeholder: "Focus State",
+  },
+};
+
+export const Error: Story = {
+  args: {
+    type: "text",
+    className: "border-red focus:border-red",
+    placeholder: "Error State",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    type: "text",
+    placeholder: "Disabled",
+    disabled: true,
+  },
+};
