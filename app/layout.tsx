6200b7bb252969c7ac7b1e92c23696794625b6d9
sik9252 <sik9252@naver.com>
2024-08-31 22:03:50 +0900

    Publish: Header 컴포넌트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
 import { QueryClientWrapper } from "@/utils";
+import { Header } from "@/components/layout";
 
 export const metadata: Metadata = {
   title: "문제저장소",
@@ -16,8 +17,13 @@ export default function RootLayout({
     <html lang="ko">
       <body>
         <QueryClientWrapper>
-          <div>헤더</div>
-          {children}
+          <div className="w-screen">
+            <Header isLogin={false} isAlreadyCreator={false} />
+            <div className="w-full max-w-[1300px] m-auto">
+              <div className="pb-[100px]" />
+              {children}
+            </div>
+          </div>
         </QueryClientWrapper>
       </body>
     </html>
--- components/layout/index.ts
+++ components/layout/index.ts
@@ -1,1 +1,2 @@
 export { Filter } from "./Filter";
+export { Header } from "./Header";
