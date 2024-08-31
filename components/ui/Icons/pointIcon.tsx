f6b5fa67f21206d024d4f1c55f5961c51f144329
sik9252 <sik9252@naver.com>
2024-08-31 21:52:44 +0900

    fix: PointIcon UI 변경

--- components/ui/Icons/pointIcon.tsx
+++ components/ui/Icons/pointIcon.tsx
@@ -1,18 +1,48 @@
-import { convertStringColorToHex } from "@/utils";
 import { SVGProps, baseColor } from "./types";
 
 export const PointIcon = ({ color = baseColor, size = "24" }: SVGProps) => (
-  <svg
-    width={size}
-    height={size}
-    viewBox="0 0 24 23"
-    fill={convertStringColorToHex(color)}
-    xmlns="http://www.w3.org/2000/svg"
-  >
+  <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
-      d="M16 15C16 13.895 12.866 13 9 13M16 15C16 16.105 12.866 17 9 17C5.134 17 2 16.105 2 15M16 15V19.937C16 21.076 12.866 22 9 22C5.134 22 2 21.077 2 19.937V15M16 15C19.824 15 23 14.013 23 13V3M9 13C5.134 13 2 13.895 2 15M9 13C4.582 13 1 12.013 1 11V6M9 4C4.582 4 1 4.895 1 6M1 6C1 7.105 4.582 8 9 8C9 9.013 12.253 10 16.077 10C19.901 10 23 9.013 23 8M23 3C23 1.895 19.9 1 16.077 1C12.254 1 9.154 1.895 9.154 3M23 3C23 4.105 19.9 5 16.077 5C12.254 5 9.154 4.105 9.154 3M9.154 3V13.166"
-      stroke={convertStringColorToHex(color)}
-      stroke-width="2"
+      d="M15.5 13C19.0899 13 22 12.1046 22 11C22 9.89543 19.0899 9 15.5 9C11.9101 9 9 9.89543 9 11C9 12.1046 11.9101 13 15.5 13Z"
+      stroke="black"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M22 15.5C22 16.605 19.09 17.5 15.5 17.5C11.91 17.5 9 16.605 9 15.5"
+      stroke="black"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M22 11V19.8C22 21.015 19.09 22 15.5 22C11.91 22 9 21.015 9 19.8V11"
+      stroke="black"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M8.5 6C12.0899 6 15 5.10457 15 4C15 2.89543 12.0899 2 8.5 2C4.91015 2 2 2.89543 2 4C2 5.10457 4.91015 6 8.5 6Z"
+      stroke="black"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M6 11C4.108 10.77 2.37 10.175 2 9M6 16C4.108 15.77 2.37 15.175 2 14"
+      stroke="black"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M6 21C4.108 20.77 2.37 20.174 2 19V4M15 6V4"
+      stroke="black"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
   </svg>
 );
