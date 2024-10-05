f35cc35001ede9dc74bc87463c1325c66c15a602
sik9252 <sik9252@naver.com>
2024-10-05 20:55:00 +0900

    feat: api request, response, authorization header로직 추가, error type 정의

---
+++ providers/http-client/httpClient.tsx
@@ -0,0 +1,32 @@
+import { createApiErrorMessage } from "./httpError";
+import axios, { AxiosResponse } from "axios";
+
+const developmentApiUrl = process.env["NEXT_PUBLIC_DEVELOPMENT_API"];
+
+const client = axios.create({
+  baseURL: developmentApiUrl,
+});
+
+async function httpClient<T>(...args: Parameters<typeof client.request>) {
+  try {
+    const res: AxiosResponse<T> = await client(...args);
+    return res.data;
+  } catch (e) {
+    if (axios.isAxiosError(e) && e.response) {
+      const errorMessage = e.response.data ? e.response.data : createApiErrorMessage(e.response.status);
+      throw new Error(errorMessage);
+    } else {
+      throw new Error("알 수 없는 오류가 발생했습니다");
+    }
+  }
+}
+
+const setBearerAuthorizationAtHttpClient = (token: string) => {
+  client.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+};
+
+const removeBearerAuthorizationAtHttpClient = () => {
+  delete client.defaults.headers.common.Authorization;
+};
+
+export { client, httpClient, setBearerAuthorizationAtHttpClient, removeBearerAuthorizationAtHttpClient };
---
+++ providers/http-client/httpError.tsx
@@ -0,0 +1,14 @@
+import { API_ERROR_MESSAGE, SERVER_ERROR_REGEX, NETWORK_ERROR_REGEX } from "@/constants";
+
+const createApiErrorMessage = (statusCode: number) => {
+  const isServerError = SERVER_ERROR_REGEX.test(statusCode.toString());
+  const isNetworkError = NETWORK_ERROR_REGEX.test(statusCode.toString());
+
+  if (isServerError) return API_ERROR_MESSAGE.serverError;
+
+  if (isNetworkError) return API_ERROR_MESSAGE.networkError;
+
+  if (statusCode in API_ERROR_MESSAGE) return API_ERROR_MESSAGE[statusCode];
+};
+
+export { createApiErrorMessage };
