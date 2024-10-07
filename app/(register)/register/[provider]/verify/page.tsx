82ef9bc9d12abc7fd4035fef951a38e35946e43c
sik9252 <sik9252@naver.com>
2024-10-07 23:39:05 +0900

    feat: 이메일 인증 대기 화면 구현, parameter의 resendToken을 이용한 재전송 로직 구현

---
+++ app/(register)/register/[provider]/verify/page.tsx
@@ -0,0 +1,15 @@
+import { AlignCenter, SidePadding } from "@/components/_shared/layout";
+import { RegisterVerify } from "@/components/register";
+import React from "react";
+
+const RegisterVerifyPage = () => {
+  return (
+    <SidePadding>
+      <AlignCenter>
+        <RegisterVerify />
+      </AlignCenter>
+    </SidePadding>
+  );
+};
+
+export default RegisterVerifyPage;
---
+++ components/register/container/registerVerify/registerVerify.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+import { Button } from "@/components/_shared/ui";
+import { useRegisterVerify } from "./useRegisterVerify";
+import { RegisterAlarmDialog } from "../../components";
+
+const RegisterVerify = () => {
+  const { handleResendEmail, email, resendEmailData, resendEmailError, isResendEmailPending } = useRegisterVerify();
+
+  return (
+    <div className="flex flex-col items-center space-y-[32px] w-[620px] m-auto">
+      <div className="heading1">회원가입</div>
+      <div className="body2 text-center">
+        입력하신 이메일로 인증 관련 메일을 전송하였습니다.
+        <br />
+        아래의 메일에 전송된 링크를 클릭하면 회원가입이 완료됩니다.
+      </div>
+      <div className="heading2 text-navy">{email}</div>
+      <div className="w-full text-center space-y-[20px]">
+        <div className="caption text-gray_03">이메일을 받지 못하셨나요?</div>
+        <Button variant="navy" size="large" onClick={handleResendEmail} disabled={isResendEmailPending}>
+          {isResendEmailPending ? "이메일 전송중" : "이메일 재전송"}
+        </Button>
+      </div>
+      {resendEmailData && <RegisterAlarmDialog message={"이메일이 재전송 되었습니다"} />}
+      {resendEmailError && <RegisterAlarmDialog message={resendEmailError?.message} />}
+    </div>
+  );
+};
+
+export { RegisterVerify };
