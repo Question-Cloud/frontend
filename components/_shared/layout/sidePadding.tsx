b53491b05db71310e88b8659cf08275adb489bce
sik9252 <sik9252@naver.com>
2024-10-07 23:30:37 +0900

    fix: sidePadding paddingTop 제거

--- components/_shared/layout/sidePadding.tsx
+++ components/_shared/layout/sidePadding.tsx
@@ -3,10 +3,7 @@ import React from "react";
 const SidePadding = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-screen">
-      <div className="w-full max-w-[1300px] m-auto">
-        <div className="pb-[100px]" />
-        {children}
-      </div>
+      <div className="w-full max-w-[1300px] m-auto">{children}</div>
     </div>
   );
 };
