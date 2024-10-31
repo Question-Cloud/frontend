b54e5ae235290f2f6b7784cf9fc0c66b170c56c1
sik9252 <sik9252@naver.com>
2024-11-01 01:32:53 +0900

    feat: 내 정보를 가져오는 API 추가

---
+++ providers/auth/api/userInfoApi.tsx
@@ -0,0 +1,15 @@
+import { httpClient } from "@/providers";
+import { useMutation } from "@tanstack/react-query";
+import { UserInfo } from "./types";
+
+function useUserInfoApi() {
+  return useMutation({
+    mutationFn: () =>
+      httpClient<UserInfo>({
+        method: "GET",
+        url: "/user/me",
+      }),
+  });
+}
+
+export { useUserInfoApi };
