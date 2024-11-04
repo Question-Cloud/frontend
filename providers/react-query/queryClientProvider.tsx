315f649b84e02368dd8004ea99ef1350a15d2de7
sik9252 <sik9252@naver.com>
2024-11-04 17:21:59 +0900

    feat: queryClient defaultOptions 추가

--- providers/react-query/queryClientProvider.tsx
+++ providers/react-query/queryClientProvider.tsx
@@ -3,7 +3,14 @@
 import { QueryClient, QueryClientProvider as ReactQuery } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      throwOnError: true,
+      retry: 1,
+    },
+  },
+});
 
 const QueryClientProvider = ({ children }: { children: React.ReactNode }) => {
   return (
