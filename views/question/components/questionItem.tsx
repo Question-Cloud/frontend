0703e6cb08e696e7be96b200f1634fc338dc8c65
sik9252 <sik9252@naver.com>
2024-11-06 00:58:05 +0900

    feat: 각 Question Item과 Item을 모은 List 컴포넌트 추가

---
+++ views/question/components/questionItem.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  Question,
+  QuestionContent,
+  QuestionTitle,
+  QuestionInfo,
+  QuestionOptions,
+  QuestionFooter,
+  Button,
+} from "@/shared";
+import { Question as QuestionData } from "@/shared/api";
+import { formatNumberWithCommas } from "@/utils";
+
+const QuestionItem = ({ question }: { question: QuestionData }) => {
+  return (
+    <Question className="w-[300px]">
+      <QuestionContent>
+        <QuestionTitle difficultyLevel={question.questionLevel} title={question.title} />
+        <QuestionInfo
+          writer={question.creatorName}
+          category={`${question.parentCategory} > ${question.childCategory}`}
+        />
+        <QuestionOptions>
+          <div className="body1">{formatNumberWithCommas(question.price)}원</div>
+        </QuestionOptions>
+      </QuestionContent>
+      <QuestionFooter>
+        <Button variant="text" className="w-full h-full">
+          상세보기
+        </Button>
+      </QuestionFooter>
+    </Question>
+  );
+};
+
+export { QuestionItem };
