e19ccd080fdeb4616bc4770c0492a94811884118
sik9252 <sik9252@naver.com>
2024-11-12 19:19:08 +0900

    feat: LoadingSpinner에 커스텀 message 추가

--- shared/components/ui/Spinners/loadingSpinner.tsx
+++ shared/components/ui/Spinners/loadingSpinner.tsx
@@ -1,9 +1,10 @@
-const LoadingSpinner = () => {
+const LoadingSpinner = ({ message }: { message?: string }) => {
   return (
     <div className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
       <div className="flex justify-center items-center h-full">
         <div className="w-[40px] h-[40px] border-4 border-navy border-t-transparent border-solid rounded-full animate-spin"></div>
       </div>
+      {message && <div className="body3 text-navy mt-[16px]">{message}</div>}
     </div>
   );
 };
