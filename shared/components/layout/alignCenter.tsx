c97d87b3ff63113812b1242f2b4f14441ca77851
sik9252 <sik9252@naver.com>
2024-11-13 13:13:09 +0900

    fix: 로그인, 회원가입 화면에서 min-h 제거

--- shared/components/layout/alignCenter.tsx
+++ shared/components/layout/alignCenter.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import { cn } from "@/utils";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const AlignCenter = ({ children, className }: { children: React.ReactNode; className?: string }) => {
+  const pathname = usePathname();
+
+  if (
+    pathname.startsWith("/register") ||
+    pathname.startsWith("/email-verification") ||
+    pathname.startsWith("/login") ||
+    pathname.startsWith("/user")
+  ) {
+    return <div className={cn("flex justify-center h-screen", className)}>{children}</div>;
+  }
+
   return <div className={cn("flex justify-center min-h-[calc(100vh-260px)]", className)}>{children}</div>;
 };
 
