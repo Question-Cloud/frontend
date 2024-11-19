cbf1a6af0dc32f2a66d1b07c6e15c720994311b8
sik9252 <sik9252@naver.com>
2024-11-19 15:15:02 +0900

    feat: Comment 컴포넌트에 사용자 상세 정보(후기, 평균 별점, 크리에이터 등록 여부) 속성 추가

--- shared/components/designsystem/Comment/comment.stories.tsx
+++ shared/components/designsystem/Comment/comment.stories.tsx
@@ -16,7 +16,7 @@ type Story = StoryObj<typeof Comment>;
 export const Default: Story = {
   render: () => (
     <Comment>
-      <CommentHeader profileImage="" userName="이지현" />
+      <CommentHeader profileImage="" userName="이지현" isCreator={false} />
       <CommentBody content="현재 Backend 1명과 함께 진행 중인 사이드 프로젝트로 수능 기출, 자작 문제를 모아 이커머스 형식으로 구성하여 사용자들이 원하는 문제를 구매하여 나만의 커스텀 문제집을 만들어 출력할 수 있는 서비스입니다." />
       <CommentFooter timestamp="2024.10.02 12:34" isWriter={true} onDelete={() => console.log("Comment deleted")} />
     </Comment>
@@ -26,7 +26,7 @@ export const Default: Story = {
 export const CommentWithRating: Story = {
   render: () => (
     <Comment>
-      <CommentHeader profileImage="" userName="이지현" />
+      <CommentHeader profileImage="" userName="이지현" isCreator={false} />
       <Rating rate={4} className="mt-[8px]" />
       <CommentBody content="프론트엔드 개발자입니다." />
       <CommentFooter timestamp="2024.10.02 12:34" isWriter={true} onDelete={() => console.log("Comment deleted")} />
@@ -38,7 +38,7 @@ export const CommentWithBadge: Story = {
   render: () => (
     <Comment>
       <div className="flex items-center">
-        <CommentHeader profileImage="" userName="이지현" className="mr-[5px]" />
+        <CommentHeader profileImage="" userName="이지현" className="mr-[5px]" isCreator={true} />
         <Badge>크리에이터</Badge>
       </div>
       <CommentBody content="현재 Backend 1명과 함께 진행 중인 사이드 프로젝트로 수능 기출, 자작 문제를 모아 이커머스 형식으로 구성하여 사용자들이 원하는 문제를 구매하여 나만의 커스텀 문제집을 만들어 출력할 수 있는 서비스입니다." />
--- shared/components/designsystem/Comment/comment.tsx
+++ shared/components/designsystem/Comment/comment.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { cn } from "@/utils/index";
 import { Avatar, AvatarImage } from "../Avatar";
+import { Badge } from "../Badge";
 
 const Comment = ({ children }: { children: React.ReactNode }) => {
   return <div className={"p-[24px] w-full"}>{children}</div>;
@@ -10,17 +11,27 @@ Comment.displayName = "Comment";
 const CommentHeader = ({
   profileImage,
   userName,
+  userCommentDetail,
+  isCreator = false,
   className,
 }: {
   profileImage: string;
   userName: string;
+  userCommentDetail?: string;
+  isCreator: boolean;
   className?: string;
 }) => (
   <div className={cn("flex items-center", className)}>
     <Avatar>
-      <AvatarImage src={profileImage ? profileImage : "default_profile_image.png"} alt={`${userName}`} />
+      <AvatarImage src={profileImage ? profileImage : "/default_profile_image.png"} alt={`${userName}`} />
     </Avatar>
-    <span className="ml-2 pt-[3px] body1">{userName}</span>
+    <div className="ml-2 space-y-[4px]">
+      <div className="flex gap-[4px]">
+        <div className="body1">{userName}</div>
+        {isCreator && <Badge className="h-[22px]">크리에이터</Badge>}
+      </div>
+      {userCommentDetail && <div className="caption text-gray_04">{userCommentDetail}</div>}
+    </div>
   </div>
 );
 CommentHeader.displayName = "CommentHeader";
